2f7769894a74c143d4668c17f1a62337ab758928
Doehyun Baek <doehyunbaek@gmail.com>
2024-08-16 10:51:12 +0900

    update crash.sh

--- crash.sh
+++ crash.sh
@@ -16,6 +16,7 @@ echo riconpacker-int: fixed by 6d2b057
 timeout 10s wizeng.x86-64-linux -mode=int ./benchmarks/riconpacker.wasm
 echo sqlgui-int: fixed by 6d2b057
 timeout 10s wizeng.x86-64-linux -mode=int ./benchmarks/sqlgui.wasm
+
 echo commanderkeen-int: fixed by 25e04ac
 timeout 10s wizeng.x86-64-linux -mode=int ./benchmarks/commanderkeen.wasm
 
@@ -23,5 +24,6 @@ echo hydro-spc: fixed by 708ea77
 timeout 10s wizeng.x86-64-linux -mode=spc ./benchmarks/hydro.wasm
 echo rtexviewer-spc: fixed by 708ea77
 timeout 10s wizeng.x86-64-linux -mode=spc ./benchmarks/rtexviewer.wasm
+
 echo mandelbrot-spc: fixed by 0b43b8
 timeout 10s wizeng.x86-64-linux -mode=spc ./benchmarks/mandelbrot.wasm
