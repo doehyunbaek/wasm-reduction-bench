7bd7c5a65108d00a4328cc4a7ba94e682491c8ae
Doehyun Baek <doehyunbaek@gmail.com>
2024-08-16 10:06:50 +0900

    echo fix commit for crashing inputs

--- crash.sh
+++ crash.sh
@@ -1,27 +1,29 @@
-echo boa
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/boa.wasm
-echo commanderkeen
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/commanderkeen.wasm
-echo funky-kart
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/funky-kart.wasm
-echo guiicons
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/guiicons.wasm
-echo jsc
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/jsc.wasm
-echo rfxgen
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/rfxgen.wasm
-echo rguilayout
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/rguilayout.wasm
-echo rguistyler
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/rguistyler.wasm
-echo riconpacker
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/riconpacker.wasm
-echo sqlgui
-timeout 10s ./engines/wizard-0d6926f -mode=int ./benchmarks/sqlgui.wasm
+echo boa-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/boa.wasm
+echo commanderkeen-int: fixed by 25e04ac
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/commanderkeen.wasm
+echo funky-kart-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/funky-kart.wasm
+echo guiicons-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/guiicons.wasm
+echo jsc-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/jsc.wasm
+echo rfxgen-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/rfxgen.wasm
+echo rguilayout-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/rguilayout.wasm
+echo rguistyler-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/rguistyler.wasm
+echo riconpacker-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/riconpacker.wasm
+echo sqlgui-int: fixed by 6d2b057
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=int ./benchmarks/sqlgui.wasm
 
-echo mandelbrot
-timeout 10s ./engines/wizard-0d6926f -mode=spc ./benchmarks/mandelbrot.wasm
-echo hydro
-timeout 10s ./engines/wizard-0d6926f -mode=spc ./benchmarks/hydro.wasm
-echo rtexviewer
-timeout 10s ./engines/wizard-0d6926f -mode=spc ./benchmarks/rtexviewer.wasm
+# TODO: check 704916c, 4745a44
+
+echo mandelbrot-spc: fixed by 708ea77, 0b43b8
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=spc ./benchmarks/mandelbrot.wasm
+echo hydro-spc: fixed by 708ea77
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=spc ./benchmarks/hydro.wasm
+echo rtexviewer-spc: fixed by 708ea77
+timeout 10s /home/don/wasm-r3/third_party/wizard-engine/bin/wizeng.x86-64-linux -mode=spc ./benchmarks/rtexviewer.wasm
