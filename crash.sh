acacdc82a0af548ef22016cce1c163d646ccea72
Doehyun Baek <doehyunbaek@gmail.com>
2024-09-21 16:20:49 +0900

    add crash scripts

--- crash.sh
+++ crash.sh
@@ -27,3 +27,53 @@ timeout 10s wizeng.x86-64-linux -mode=spc ./benchmarks/rtexviewer.wasm
 
 echo mandelbrot-spc: fixed by 0b43b8
 timeout 10s wizeng.x86-64-linux -mode=spc ./benchmarks/mandelbrot.wasm
+
+./engines/wasmtime-14.0.4 benchmarks/wasmtime#7558.wasm
+
+./engines/iwasm-0b0af1b --heap-size=0 -f main benchmarks/wamr#2450.wasm
+
+num=2555; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+num=2555; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+./engines/wamrc-0b0af1b -o wamr#2557.wasm.aot ./benchmarks/wamr#2557.wasm
+
+num=2561; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+num=2677; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+num=2690; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+num=2720; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+./engines/wamrc-0b0af1b -o wamr#2789.wasm.aot ./benchmarks/wamr#2789.wasm
+
+num=2861; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+num=2862; ./engines/wamrc-0b0af1b -o wamr#$num.wasm.aot ./benchmarks/wamr#$num.wasm && ./engines/iwasm-0b0af1b --heap-size=o -f main wamr#$num.wasm.aot
+
+num=2812; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=2814; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=2815; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=2988; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=2996; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=2997; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=2999; ./engines/wasmedge-0.13.4 --reactor ./benchmarks/wasmedge#$num.wasm main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=3018; ./engines/wasmedge-0.13.4 compile ./benchmarks/wasmedge#$num.wasm wasmedge#$num.wasm.so && timeout 1s ./engines/wasmedge-0.13.4 wasmedge#$num.wasm.so main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=3019; ./engines/wasmedge-0.13.4 compile ./benchmarks/wasmedge#$num.wasm wasmedge#$num.wasm.so && timeout 1s ./engines/wasmedge-0.13.4 wasmedge#$num.wasm.so main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=3057; ./engines/wasmedge-0.13.4 compile ./benchmarks/wasmedge#$num.wasm wasmedge#$num.wasm.so && timeout 1s ./engines/wasmedge-0.13.4 wasmedge#$num.wasm.so main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=3063; ./engines/wasmedge-0.13.4 compile ./benchmarks/wasmedge#$num.wasm wasmedge#$num.wasm.so && timeout 1s ./engines/wasmedge-0.13.4 wasmedge#$num.wasm.so main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=3068; ./engines/wasmedge-0.13.4 compile ./benchmarks/wasmedge#$num.wasm wasmedge#$num.wasm.so && timeout 1s ./engines/wasmedge-0.13.4 wasmedge#$num.wasm.so main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
+
+num=3076; ./engines/wasmedge-0.13.4 compile ./benchmarks/wasmedge#$num.wasm wasmedge#$num.wasm.so && timeout 1s ./engines/wasmedge-0.13.4 wasmedge#$num.wasm.so main; wasmtime --invoke main ./benchmarks/wasmedge#$num.wasm
